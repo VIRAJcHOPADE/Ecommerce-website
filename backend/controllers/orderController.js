724235bd880ab79cb362835bf54a3e2f2a2e1c7b
VIRAJcHOPADE <89187084+VIRAJcHOPADE@users.noreply.github.com>
2023-03-30 07:00:55 +0530

    Order Admin

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,6 +5,7 @@ const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const ErrorHandler = require("../utils/errorhandler");
 
 // Create new Order
+
 exports.newOrder = catchAsyncErrors(async (req, res, next) => {
   const {
     shippingInfo,
@@ -34,8 +35,7 @@ exports.newOrder = catchAsyncErrors(async (req, res, next) => {
   });
 });
 
-
-// get Single Order
+// get Single Order   // ERROR
 exports.getSingleOrder = catchAsyncErrors(async (req, res, next) => {
   const order = await Order.findById(req.params.id).populate(
     "user",
@@ -43,7 +43,7 @@ exports.getSingleOrder = catchAsyncErrors(async (req, res, next) => {
   );
 
   if (!order) {
-    return next(new ErrorHandler("Order not found with this Id", 404));
+    return next(new ErrorHander("Order not found with this Id", 404));
   }
 
   res.status(200).json({
@@ -52,14 +52,83 @@ exports.getSingleOrder = catchAsyncErrors(async (req, res, next) => {
   });
 });
 
-
-// get logged in user Orders
+// get logged in user  Orders
 exports.myOrders = catchAsyncErrors(async (req, res, next) => {
-  
-  const orders = await Order.find({ user: req.user._id});
- 
+  const orders = await Order.find({ user: req.user._id });
+
   res.status(200).json({
     success: true,
     orders,
+  });
+});
+
+// get all Orders -- Admin
+exports.getAllOrders = catchAsyncErrors(async (req, res, next) => {
+  const orders = await Order.find();
+
+  let totalAmount = 0;
+
+  orders.forEach((order) => {
+    totalAmount += order.totalPrice;
+  });
+
+  res.status(200).json({
+    success: true,
+    totalAmount,
+    orders,
+  });
+});
+
+// update Order Status -- Admin
+exports.updateOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (!order) {
+    return next(new ErrorHander("Order not found with this Id", 404));
+  }
+
+  if (order.orderStatus === "Delivered") {
+    return next(new ErrorHander("You have already delivered this order", 400));
+  }
+
+  if (req.body.status === "Shipped") {
+    order.orderItems.forEach(async (o) => {
+      await updateStock(o.product, o.quantity);
+    });
+  }
+  order.orderStatus = req.body.status;
+
+  if (req.body.status === "Delivered") {
+    order.deliveredAt = Date.now();
+  }
+
+  await order.save({ validateBeforeSave: false });
+  res.status(200).json({
+    success: true,
+  });
+});
+
+async function updateStock(id, quantity) {
+  const product = await Product.findById(id);
+
+  product.Stock -= quantity;
+
+  await product.save({ validateBeforeSave: false });
+}
+
+// delete Orders -- Admin
+exports.deleteOrder = catchAsyncErrors(async (req, res, next) => {
+  
+  // const orders = await Order.findById(req.params.id);
+    const order = await Order.findByIdAndDelete(req.params.id);
+ 
+  if (!order) {
+    return next (new ErrorHandler("Order not found with this Id",404));
+  }
+
+  // await order.remove();
+
+  res.status(200).json({
+    success: true,
   });
 });
