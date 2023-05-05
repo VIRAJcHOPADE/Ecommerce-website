e507792dcb847edda2f4940137ddb92837eed947
VIRAJcHOPADE <89187084+VIRAJcHOPADE@users.noreply.github.com>
2023-05-05 21:08:45 +0530

    userProfile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import './App.css';
 import { useSelector } from "react-redux";
+import Profile from "./component/User/Profile.jsx";
+// import ProtectedRoute from "./component/Route/ProtectedRoute";
+
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import WebFont from "webfontloader";
 import Header from "./component/layout/Header/Header"
 import Footer from "./component/layout/Footer/Footer"
 import Home from "./component/Home/Home"
-import Loader from "./component/layout/Loader/Loader";
+// import Loader from "./component/layout/Loader/Loader";
 import ProductDetails from "./component/product/ProductDetails.js"
 import Products from "./component/product/Products.js"
 import Search from "./component/product/Search.js"
@@ -42,7 +45,7 @@ function App() {
       <Route  path="/products/:keyword" element={<Products />} />
       <Route exact path="/search" element={<Search/>} />
       <Route exact path="/login" element={<LoginSignUp/>} />
-      
+      <Route exact path="/account" element={<Profile/>} />
       </Routes>
       <Footer />
     </Router>
---
+++ frontend/src/component/User/Profile.jsx
@@ -0,0 +1,65 @@
+import React, { Fragment, useEffect } from "react";
+import { useSelector } from "react-redux";
+import MetaData from "../layout/MetaData";
+import Loader from "../layout/Loader/Loader";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+import "./Profile.css";
+
+const Profile = () => {
+  const { user, loading, isAuthenticated } = useSelector((state) => state.user);
+ const Navigate = useNavigate();
+  const checkValidation = ()=>{
+        if (isAuthenticated === false) {
+               Navigate("/login");
+            }
+  }
+  
+  const history = useNavigate();
+
+  useEffect(() => {
+    if (isAuthenticated === false) {
+      history("/login");
+    }
+  }, [history, isAuthenticated]);
+  return (
+    <Fragment>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Fragment>
+          <MetaData title={`${user.name}'s Profile`} />
+          <div className="profileContainer">
+            <div>
+              <h1>My Profile</h1>
+              <img src={user?.avatar?.url || "https://yt3.googleusercontent.com/-CFTJHU7fEWb7BYEb6Jh9gm1EpetvVGQqtof0Rbh-VQRIznYYKJxCaqv_9HeBcmJmIsp2vOO9JU=s900-c-k-c0x00ffffff-no-rj"} alt={user.name} />
+              <Link to="/me/update">Edit Profile</Link>
+            </div>
+            <div>
+              <div>
+                <h4>Full Name</h4>
+                <p>{user.name}</p>
+              </div>
+              <div>
+                <h4>Email</h4>
+                <p>{user.email}</p>
+              </div>
+              <div>
+                <h4>Joined On</h4>
+                <p>{String(user.createdAt).substr(0, 10)}</p>
+              </div>
+
+              <div>
+                <Link to="/orders">My Orders</Link>
+                <Link to="/password/update">Change Password</Link>
+              </div>
+            </div>
+          </div>
+        </Fragment>
+      )}
+    </Fragment>
+  );
+};
+
+export default Profile;
