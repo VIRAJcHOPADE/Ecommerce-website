6c8ae788bb4ac3b0b7859a7d679b34feefec7635
VIRAJcHOPADE <89187084+VIRAJcHOPADE@users.noreply.github.com>
2023-05-11 15:56:23 +0530

    orderList

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -91,22 +91,53 @@ exports.getProductDetails = catchAsyncErrors( async(req,res,next)=>{
 
 exports.updateProduct = catchAsyncErrors( async (req,res,next)=>{
     
-    let product = Product.findById(req.params.id);
-    
-    if(!product){
-        return next(new ErrorHandler("Product not found",404)) ;
+     let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return next(new ErrorHander("Product not found", 404));
+  }
+
+  // Images Start Here
+  let images = [];
+
+  if (typeof req.body.images === "string") {
+    images.push(req.body.images);
+  } else {
+    images = req.body.images;
+  }
+
+  if (images !== undefined) {
+    // Deleting Images From Cloudinary
+    for (let i = 0; i < product.images.length; i++) {
+      await cloudinary.v2.uploader.destroy(product.images[i].public_id);
     }
 
-    product = await Product.findByIdAndUpdate(req.params.id,req.body,{
-            new:true,
-            runvalidators:true,
-            useFindAndModify:false
-        });
+    const imagesLinks = [];
 
-        res.status(200).json({
-            success:true,
-            product
-        })
+    for (let i = 0; i < images.length; i++) {
+      const result = await cloudinary.v2.uploader.upload(images[i], {
+        folder: "products",
+      });
+
+      imagesLinks.push({
+        public_id: result.public_id,
+        url: result.secure_url,
+      });
+    }
+
+    req.body.images = imagesLinks;
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
 });
 
 // Delete Product
