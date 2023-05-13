d765e274e3ad345ce41b35e95d3da170e6fa82b9
VIRAJcHOPADE <89187084+VIRAJcHOPADE@users.noreply.github.com>
2023-05-13 17:14:28 +0530

    about

--- frontend/src/component/layout/Contact/Contact.js
+++ frontend/src/component/layout/Contact/Contact.js
@@ -6,7 +6,7 @@ const Contact = () => {
   return (
     <div className="contactContainer">
       <a className="mailBtn" href="mailto:mymailforabhi@gmail.com">
-        <Button>Contact: virajchopade527@gmail.com</Button>
+        <Button>Contact:virajchopade527@gmail.com</Button>
       </a>
     </div>
   );
