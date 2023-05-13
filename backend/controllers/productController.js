cf4f4b5373b7e32a15d418ed76992f5c2d1f50ed
VIRAJcHOPADE <89187084+VIRAJcHOPADE@users.noreply.github.com>
2023-05-13 17:03:42 +0530

    user

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -238,7 +238,13 @@ exports.deleteReview = catchAsyncErrors(async (req, res, next) => {
         avg += rev.rating;
     });
 
-    const ratings = avg / reviews.length;
+     let ratings = 0;
+
+  if (reviews.length === 0) {
+    ratings = 0;
+  } else {
+    ratings = avg / reviews.length;
+  }
 
     const numOfReviews = reviews.length;
 
